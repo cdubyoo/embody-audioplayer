5fd33f38f118cf9aaf2a3aebb73c9e367fe26da9
cdubyoo <chungwongg@gmail.com>
2021-09-04 01:49:35 -0700

    switch db to psql

--- audioplayer/audioplayer/settings.py
+++ audioplayer/audioplayer/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'audioplayer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'audioplayer_db',
+        'USER': 'jaywong',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
