3c792a151fd3a0b679b9462b439c1634c7189f39
cdubyoo <chungwongg@gmail.com>
2021-09-07 18:38:59 -0700

    put comments into  its own component and update state on commenting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,39 +6,11 @@ import audiotrack1 from './audiotrack1.wav';
 import Comment from "./Comment";
 
 function App() {
-  const [audio, setAudio] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios('/api/audios');
-      setAudio(result.data); 
-    };
-    fetchData();
-  }, [])
-
-  // turn time stamp seconds into mm:ss
-  function timeStampConvert(seconds) {
-    return new Date(seconds * 1000).toISOString().substr(14,5);
-  }
 
   return (
     <div className="container">
       <Player url={audiotrack1} />
       <Comment />
-      {audio.map(item => (
-        <li key={item.id}>
-          {item.title} by {item.artist}
-          <span>
-            {item.comments.map(comment => (
-              <li>
-                {comment.user} at {timeStampConvert(comment.timestamp_seconds)}
-                <br></br>
-                <span>{comment.body}</span>
-              </li>
-            ))}
-          </span>
-        </li>
-      ))}
     </div>
   );
 }
--- frontend/src/Comment.js
+++ frontend/src/Comment.js
@@ -5,12 +5,24 @@ import Cookies from 'js-cookie'
 const csrftoken = Cookies.get('csrftoken') // Cookies from Django Domain
 
 const Comment = () => {
-    // const [audio, setAudio] = useState([])
+    const [audio, setAudio] = useState([])
     const [user, setUser] = useState('Anon')
     const [body, setBody] = useState('')
     const [timestamp, setTimestamp] = useState(0)
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios('/api/audios');
+            setAudio(result.data); 
+        };
+        fetchData();
+    }, [audio])
     
+
+    // turn time stamp seconds into mm:ss
+  function timeStampConvert(seconds) {
+    return new Date(seconds * 1000).toISOString().substr(14,5);
+  }
     const handleSubmit = (e) => {
         e.preventDefault()
         axios.post('/api/comments/',  
@@ -25,6 +37,10 @@ const Comment = () => {
 
     return(
         <div>
+             {audio.map(item => (
+                 <span>{item.title} by {item.artist}</span>
+             ))}
+            
             <form onSubmit={handleSubmit}>
                 <label type="submit">
                     <input placeholder="Name" type="text" name="name" onChange={e => setUser(e.target.value)}/>
@@ -32,6 +48,19 @@ const Comment = () => {
                 </label>
                 <button type="submit">Add</button>
             </form>
+            <div>
+                 {audio.map(item => (
+          <span>
+            {item.comments.map(comment => (
+              <li key={item.comments.id}>
+                {comment.user} at {timeStampConvert(comment.timestamp_seconds)}
+                <br></br>
+                <span>{comment.body}</span>
+              </li>
+            ))}
+          </span>
+      ))}
+            </div>
         </div>
     );
 };
