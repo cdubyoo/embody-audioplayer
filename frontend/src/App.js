f49df55310ee5378bdb5dc9183a8bdec49411c39
cdubyoo <chungwongg@gmail.com>
2021-09-09 01:24:42 -0700

    moved components together

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
-import React, { useEffect, useState } from "react";
 import "./index.css";
-import axios from "axios";
 import Player from "./Player";
 import audiotrack1 from './audiotrack1.wav';
-import Comment from "./Comment";
 
 function App() {
 
   return (
     <div className="container">
       <Player url={audiotrack1} />
-      <Comment />
     </div>
   );
 }
--- frontend/src/Player.js
+++ frontend/src/Player.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
-import WaveSurfer from "wavesurfer.js";
+import React, { useEffect, useRef, useState } from "react"
+import WaveSurfer from "wavesurfer.js"
+import axios from "axios";
+import Cookies from 'js-cookie'
 
 // options for the audio player
 const formWaveSurferOptions = () => ({
@@ -11,26 +13,26 @@ const formWaveSurferOptions = () => ({
     barWidth: 3,
     barRadius: 3,
     responsive: true,
-  });
+  })
 
 // audio player that takes in url of audio
 const Player = ({ url }) => {
     // set ref to elements to replace old ones when react rerenders
-    const waveformRef = useRef(null);
-    const wavesurfer = useRef(null);
+    const waveformRef = useRef(null)
+    const wavesurfer = useRef(null)
     // set initial states for play and volume
-    const [playing, setPlay] = useState(false);
-    const [volume, setVolume] = useState(0.5);
+    const [playing, setPlay] = useState(false)
+    const [volume, setVolume] = useState(0.5)
 
     // create new waveSurfer instance on component mount and when url changes
     useEffect(() => {
         // stops play on reload
-        setPlay(false);
+        setPlay(false)
         // point back to the option settings
         const options = formWaveSurferOptions(waveformRef.current);
         wavesurfer.current = WaveSurfer.create(options);
 
-        wavesurfer.current.load(url);
+        wavesurfer.current.load(url)
 
         wavesurfer.current.on('ready', function() {
         
@@ -40,32 +42,82 @@ const Player = ({ url }) => {
 
         // maintain volume settings on reload
         if (wavesurfer.current) {
-            wavesurfer.current.setVolume(volume);
-            setVolume(volume);
+            wavesurfer.current.setVolume(volume)
+            setVolume(volume)
         }
-        });
+        })
 
         // remove events, elements and disconnects when component unmount
-        return () => wavesurfer.current.destroy();
-    }, [url]);
+        return () => wavesurfer.current.destroy()
+    }, [url])
 
     // toggles play and pause
     const handlePlayPause = () => {
         setPlay(!playing);
         // toggles ref as well
-        wavesurfer.current.playPause();
-    };
+        wavesurfer.current.playPause()
+    }
 
     // changes volume state
     const onVolumeChange = e => {
         const { target } = e;
-        const newVolume = +target.value;
+        const newVolume = +target.value
         // checks for change in volume, and sets state and points ref
         if (newVolume) {
-        setVolume(newVolume);
-        wavesurfer.current.setVolume(newVolume);
+        setVolume(newVolume)
+        wavesurfer.current.setVolume(newVolume)
         }
-    };
+    }
+
+    
+    // ------------------------------ COMMENTS -------------------------------------
+
+    const csrftoken = Cookies.get('csrftoken') // Cookies from Django Domain
+
+    const [audio, setAudio] = useState([])
+    const [user, setUser] = useState('Anon')
+    const [body, setBody] = useState('')
+    const [timestamp, setTimestamp] = useState(0)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios('/api/audios');
+            setAudio(result.data); 
+        };
+        fetchData();
+    }, [])
+
+    const getTimestamp = () => { if (wavesurfer.current != null) {  
+            wavesurfer.current.on('audioprocess', () => {
+                wavesurfer.getCurrentTime()
+            })
+        } else {
+            return 0
+        }
+    }
+
+    // checking for what its returning
+    console.log(getTimestamp())
+
+    // turn time stamp seconds into mm:ss
+    function timeStampConvert(seconds) {
+        return new Date(seconds * 1000).toISOString().substr(14,5);
+        }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // chain api requests to update audio state to re-render comment section
+        axios.post('/api/comments/',  
+            {
+                audio: 1,
+                user: user,
+                body: 'hi',
+                timestamp_seconds: timeStampConvert(timestamp)
+            }, {headers: { 'X-CSRFToken': csrftoken }})
+            .then(
+                axios.get('/api/audios')
+                    .then(res => setAudio(res.data))
+            )
+    }
 
     return (
         <div>
@@ -85,8 +137,34 @@ const Player = ({ url }) => {
                 />
                 <label htmlFor="volume">Volume</label>
             </div>
+            <div>
+             {audio.map(item => (
+                 <span>{item.title} by {item.artist}</span>
+             ))}
+            <form onSubmit={handleSubmit}>
+                <label type="submit">
+                    <input placeholder="Name" type="text" name="name" onChange={e => setUser(e.target.value)}/>
+                    <input onChange={()=> setTimestamp(getTimestamp())} placeholder="Leave a comment" type="text" name="comment" required onChange={e => setBody(e.target.value)} />
+                </label>
+                <button type="submit">Add</button>
+            </form>
+            <div>
+                 {audio.map(item => (
+          <span>
+            {item.comments.map(comment => (
+              <span key={item.comments.id}>
+                {comment.user} at {comment.timestamp_seconds}
+                <br></br>
+                <li>{comment.body}</li>
+              </span>
+            ))}
+          </span>
+      ))}
+            </div>
         </div>
-        );
+        </div>
+        
+        )
 }
 
 export default Player
